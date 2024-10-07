fa2b2ce5ce5a67660b90bc4d486801df6dc8d927
Javier Trujillo <rivera.dev@outlook.com>
2024-10-06 23:49:34 -0600

    Add - Print all calculator methods results from main()

--- src/Calculator.java
+++ src/Calculator.java
@@ -34,5 +34,9 @@ public class Calculator {
         //Create object.
         Calculator myCalculator = new Calculator();
         System.out.println(myCalculator.add(5,7));
+        System.out.println(myCalculator.subtract(45,11));
+        System.out.println(myCalculator.multiply(5,5));
+        System.out.println(myCalculator.divide(5,5));
+        System.out.println(myCalculator.modulo(5,5));
     }
 }
